bdbe9e139858e7e07fe26f41cd7a1b1253f67a7d
rudibarr <rudibarr@sheridancollege.ca>
2024-01-10 15:10:07 -0500

    Tank done with horizontal movement

---
+++ Assets/Week 1/Scripts/Mover.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    float speed = 3f;
+   public GameObject misslePrefab; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxis("Horizontal");
+        transform.Translate(direction* Time.deltaTime, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(misslePrefab);
+        }
+    }
+}
