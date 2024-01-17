272e9eaeeee26f869d75bf31f44dacfed5cd3861
Xiomaraze <allie.arnott@gmail.com>
2024-01-17 10:12:34 -0500

    made tank prefab in week 1 created walls in week 2 task 1 added physics to walls and tank

--- Assets/Week 1/Scripts/Missile.cs
+++ Assets/Week 1/Scripts/Missile.cs
@@ -6,14 +6,25 @@ using UnityEngine.UIElements;
 public class Missile : MonoBehaviour
 {
     // Start is called before the first frame update
+    Rigidbody2D rigidbody;
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
     float speed = 5f;
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(1 * speed * Time.deltaTime, 0, 0);
+        //transform.Translate(1 * speed * Time.deltaTime, 0, 0);
+    }
+    private void FixedUpdate()
+    {
+        Vector2 direction = new Vector2(speed * Time.deltaTime, 0);
+        rigidbody.MovePosition(rigidbody.position + direction);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("missile collision");
     }
 }
