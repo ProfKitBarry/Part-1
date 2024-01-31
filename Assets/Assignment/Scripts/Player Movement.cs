ddf8c4e7dc7b913e99b3421840a4d514a2dca1c5
Xiomaraze <allie.arnott@gmail.com>
2024-01-30 19:00:04 -0500

    Created Player and background instantiated physics added player movement with input

--- Assets/Assignment/Scripts/Player Movement.cs
+++ Assets/Assignment/Scripts/Player Movement.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    Rigidbody2D playerBody;
+    Vector2 movement;
+    public float speed;
     void Start()
     {
-        
+        playerBody = GetComponent<Rigidbody2D>();
     }
 
+    private void FixedUpdate()
+    {
+        movement = new Vector2(Input.GetAxis("Horizontal"), 0) * speed * Time.deltaTime;
+        playerBody.AddForce(movement);
+    }
     // Update is called once per frame
     void Update()
     {
